ebeacfb2e95c315856113b2a25bccd074fba7c70
Fotonchik <70895760+Fotonchik@users.noreply.github.com>
2020-10-17 13:38:12 +0300

    Add files via upload

---
+++ Computerfirm/Models/Orders.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Computerfirm.Models
+{
+    public class Orders
+    {
+        public long ID { get; set; }
+        public DateTime OrderData { get; set; }
+        public DateTime DueData { get; set; }
+        public int ShareofPrepayment { get; set; }
+        public Boolean  PaymentMark { get; set; }
+        public Boolean  ExecutionMark{ get; set; }
+        public DateTime GeneralWarranttyPeriod { get; set; }
+        public int TotalCost { get; set; }
+        public DbSet<Customers> CustomerID { get; set; }
+        public DbSet<Components> ComponentID1 { get; set; }
+        public DbSet<Components> ComponentID2 { get; set; }
+        public DbSet<Components> ComponentID3 { get; set; }
+        public DbSet<Services> ServicesID1 { get; set; }
+        public DbSet<Services> ServicesID2 { get; set; }
+        public DbSet<Services> ServicesID3 { get; set; }
+
+
+    }
+}
---
+++ Computerfirm/Models/TypesofСomponents.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Computerfirm.Models
+{
+    public class TypesofСomponents
+    {
+        public long TypesofСomponentsID { get; set; }
+        public string NameTypesofComponents { get; set; }
+        public string Description { get; set; }
+        public DbSet<TypesofСomponents> TypesofСomponent { get; set; }
+
+    }
+}
